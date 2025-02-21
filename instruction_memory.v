3ffa9e78131657d917c33259cc08b5cf3bb1f20c
Rithvik <sairithvik.achutuni@students.iiit.ac.in>
2025-02-21 23:46:14 +0530

    intruction mem is done

---
+++ instruction_memory.v
@@ -0,0 +1,23 @@
+module instruction_mem (
+  input clk,
+  input rst,
+  input [31:0] addr,
+  output reg [31:0] inst
+);
+
+  reg [31:0] mem [63:0];
+  integer i;
+
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      for (i = 0; i < 64; i = i+1) begin
+        mem[i] <= 32'b0;
+      end
+    end
+
+    else begin
+      inst <= mem[addr];
+    end
+    
+  end
+endmodule
