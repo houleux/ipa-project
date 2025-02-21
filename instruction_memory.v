18f67845db11b8233648f7cad56d6c77e5331212
Rithvik <sairithvik.achutuni@students.iiit.ac.in>
2025-02-22 00:25:58 +0530

    changed name lol

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,23 +1,22 @@
-module instruction_mem (
-  input clk,
-  input rst,
-  input [31:0] addr,
-  output reg [31:0] inst
+module instruction_memory (
+    input clk,
+    input rst,
+    input [31:0] addr,
+    output reg [31:0] inst
 );
 
-  reg [31:0] mem [63:0];
+  reg [31:0] mem[63:0];
   integer i;
 
   always @(posedge clk or posedge rst) begin
     if (rst) begin
-      for (i = 0; i < 64; i = i+1) begin
+      for (i = 0; i < 64; i = i + 1) begin
         mem[i] <= 32'b0;
       end
-    end
-
-    else begin
+    end else begin
       inst <= mem[addr];
     end
-    
+
   end
 endmodule
+
